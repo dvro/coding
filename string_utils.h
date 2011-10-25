8a87794088eee4f38910a4649e6855ec89016a00
Dayvid Victor <victor.dvro@gmail.com>
2011-10-25 09:05:07 -0300

    string_utils.h

---
+++ string_utils.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+
+
+int str_to_int (char *str, int value *);
+
+int int_to_str (int value, char **str);
+
+int reverse_words (char **str);
+
+int large_block (char *str, int *index);
+
+
