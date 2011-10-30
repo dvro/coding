b2700292527c293c9ccd29234db5a69b59fa690b
Dayvid Victor <victor.dvro@gmail.com>
2011-10-30 15:51:40 -0300

    test_string_utils.c - reverse_words tested

--- test/test_string_utils.c
+++ test/test_string_utils.c
@@ -1,82 +1,82 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <check.h>
-#include <string_utils.h>
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <check.h>
+	#include <string_utils.h>
 
-START_TEST (test_str_to_int)
-{
-	int value = -1;
-	int status = str_to_int("1234", &value);
-	fail_unless (status == 1);
-	fail_unless (value == 1234);
-}
-END_TEST
+	START_TEST (test_str_to_int)
+	{
+		int value = -1;
+		int status = str_to_int("1234", &value);
+		fail_unless (status == 1);
+		fail_unless (value == 1234);
+	}
+	END_TEST
 
-START_TEST (test_str_to_int_negative_value)
-{
-	int value = -1;
-	int status = str_to_int("-1234", &value);
-	fail_unless (status == 1);
-	fail_unless (value == -1234);
-}
-END_TEST
+	START_TEST (test_str_to_int_negative_value)
+	{
+		int value = -1;
+		int status = str_to_int("-1234", &value);
+		fail_unless (status == 1);
+		fail_unless (value == -1234);
+	}
+	END_TEST
 
-START_TEST (test_str_to_int_one_string)
-{
-	int value = -1;
-	int status = str_to_int("1", &value);
-	fail_unless (status == 1);
-	fail_unless (value == 1);
-}
-END_TEST
+	START_TEST (test_str_to_int_one_string)
+	{
+		int value = -1;
+		int status = str_to_int("1", &value);
+		fail_unless (status == 1);
+		fail_unless (value == 1);
+	}
+	END_TEST
 
-START_TEST (test_str_to_int_null_string)
-{
-	int value = -1;
-	int status = str_to_int(NULL, &value);
-	fail_unless (status == 0);
-	fail_unless (value == -1);
-}
-END_TEST
+	START_TEST (test_str_to_int_null_string)
+	{
+		int value = -1;
+		int status = str_to_int(NULL, &value);
+		fail_unless (status == 0);
+		fail_unless (value == -1);
+	}
+	END_TEST
 
-START_TEST (test_str_to_int_empty_string)
-{
-	int value = -1;
-	int status = str_to_int("", &value);
-	fail_unless (status == 0);
-	fail_unless (value == -1);
-}
-END_TEST
+	START_TEST (test_str_to_int_empty_string)
+	{
+		int value = -1;
+		int status = str_to_int("", &value);
+		fail_unless (status == 0);
+		fail_unless (value == -1);
+	}
+	END_TEST
 
-START_TEST (test_str_to_int_invalid_string)
-{
-	int value = -1;
-	int status = str_to_int("-123a4", &value);
-	fail_unless (status == 0);
-	fail_unless (value == -1);
-}
-END_TEST
+	START_TEST (test_str_to_int_invalid_string)
+	{
+		int value = -1;
+		int status = str_to_int("-123a4", &value);
+		fail_unless (status == 0);
+		fail_unless (value == -1);
+	}
+	END_TEST
 
-START_TEST (test_int_to_str)
-{
-	int value = 123;
-	char *expected = "123";
-	char *actual = calloc(10, sizeof(char));
-	int status = int_to_str(value, &actual);
-	fail_unless(status == 1, "status = %d", status);
-	fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
-	free(actual);
-}
-END_TEST
+	START_TEST (test_int_to_str)
+	{
+		int value = 123;
+		char *expected = "123";
+		char *actual = calloc(10, sizeof(char));
+		int status = int_to_str(value, &actual);
+		fail_unless(status == 1, "status = %d", status);
+		fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
+		free(actual);
+	}
+	END_TEST
 
-START_TEST (test_int_to_str_negative_value)
-{
-	int value = -123;
-	char *expected = "-123";
-	char *actual = calloc(10, sizeof(char));
-	int status = int_to_str(value, &actual);
-	fail_unless(status == 1, "status = %d", status);
-	fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
+	START_TEST (test_int_to_str_negative_value)
+	{
+		int value = -123;
+		char *expected = "-123";
+		char *actual = calloc(10, sizeof(char));
+		int status = int_to_str(value, &actual);
+		fail_unless(status == 1, "status = %d", status);
+		fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
 	free(actual);
 }
 END_TEST
@@ -88,6 +88,67 @@ START_TEST (test_int_to_str_zero_value)
 	char *actual = calloc(10, sizeof(char));
 	int status = int_to_str(value, &actual);
 	fail_unless(status == 1, "status = %d", status);
+	fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
+	free(actual);
+}
+END_TEST
+
+START_TEST (test_reverse_words)
+{
+	char *actual = strdup("my name is dayvid");
+	char *expected = "dayvid is name my";
+	int status = reverse_words(actual);
+	fail_unless(status == 1);
+	fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
+	free(actual);
+}
+END_TEST
+
+START_TEST (test_reverse_words_no_space)
+{
+	char *actual = strdup("12345");
+	char *expected = "12345";
+	int status = reverse_words(actual);
+	fail_unless(status == 1);
+	fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
+	free(actual);
+}
+END_TEST
+
+START_TEST (test_reverse_words_one_char_string)
+{
+	char *actual = strdup("a");
+	char *expected = "a";
+	int status = reverse_words(actual);
+	fail_unless(status == 1);
+	fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
+	free(actual);
+}
+END_TEST
+
+START_TEST (test_reverse_words_null_string)
+{
+	char *actual = NULL;
+	int status = reverse_words(actual);
+	fail_unless(status == 0, "status = %d", status);
+}
+END_TEST
+
+START_TEST (test_reverse_words_empty_string)
+{
+	char *actual = strdup("");
+	int status = reverse_words(actual);
+	fail_unless(status == 1, "status = %d", status);
+	free(actual);
+}
+END_TEST
+
+START_TEST (test_reverse_words_lots_of_spaces)
+{
+	char *actual = strdup(" 1  2   3    4     ");
+	char *expected = "     4    3   2  1 ";
+	int status = reverse_words(actual);
+	fail_unless(status == 1);
 	fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
 	free(actual);
 }
@@ -111,6 +172,15 @@ Suite *get_suite(void)
 	tcase_add_test(tc_int_to_str, test_int_to_str_negative_value);
 	tcase_add_test(tc_int_to_str, test_int_to_str_zero_value);
 	suite_add_tcase(s, tc_int_to_str);
+
+	TCase *tc_reverse_words = tcase_create("tcase_reverse_words");
+	tcase_add_test(tc_reverse_words, test_reverse_words);
+	tcase_add_test(tc_reverse_words, test_reverse_words_no_space);
+	tcase_add_test(tc_reverse_words, test_reverse_words_one_char_string);
+	tcase_add_test(tc_reverse_words, test_reverse_words_null_string);
+	tcase_add_test(tc_reverse_words, test_reverse_words_empty_string);
+	tcase_add_test(tc_reverse_words, test_reverse_words_lots_of_spaces);
+	suite_add_tcase(s, tc_reverse_words);
 	
 	return s;
 }
