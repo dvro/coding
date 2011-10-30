581b2bcf1c5c03b2ac1381ed3fc923975fa15b84
Dayvid Victor <victor.dvro@gmail.com>
2011-10-30 18:53:00 -0300

    remove_quotes tests added

--- test/test_string_utils.c
+++ test/test_string_utils.c
@@ -1,83 +1,83 @@
-	#include <stdio.h>
-	#include <stdlib.h>
-	#include <check.h>
-	#include <string_utils.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <check.h>
+#include <string_utils.h>
 
-	START_TEST (test_str_to_int)
-	{
-		int value = -1;
-		int status = str_to_int("1234", &value);
-		fail_unless (status == 1);
-		fail_unless (value == 1234);
-	}
-	END_TEST
+START_TEST (test_str_to_int)
+{
+	int value = -1;
+	int status = str_to_int("1234", &value);
+	fail_unless (status == 1);
+	fail_unless (value == 1234);
+}
+END_TEST
 
-	START_TEST (test_str_to_int_negative_value)
-	{
-		int value = -1;
-		int status = str_to_int("-1234", &value);
-		fail_unless (status == 1);
-		fail_unless (value == -1234);
-	}
-	END_TEST
+START_TEST (test_str_to_int_negative_value)
+{
+	int value = -1;
+	int status = str_to_int("-1234", &value);
+	fail_unless (status == 1);
+	fail_unless (value == -1234);
+}
+END_TEST
 
-	START_TEST (test_str_to_int_one_string)
-	{
-		int value = -1;
-		int status = str_to_int("1", &value);
-		fail_unless (status == 1);
-		fail_unless (value == 1);
-	}
-	END_TEST
+START_TEST (test_str_to_int_one_string)
+{
+	int value = -1;
+	int status = str_to_int("1", &value);
+	fail_unless (status == 1);
+	fail_unless (value == 1);
+}
+END_TEST
 
-	START_TEST (test_str_to_int_null_string)
-	{
-		int value = -1;
-		int status = str_to_int(NULL, &value);
-		fail_unless (status == 0);
-		fail_unless (value == -1);
-	}
-	END_TEST
+START_TEST (test_str_to_int_null_string)
+{
+	int value = -1;
+	int status = str_to_int(NULL, &value);
+	fail_unless (status == 0);
+	fail_unless (value == -1);
+}
+END_TEST
 
-	START_TEST (test_str_to_int_empty_string)
-	{
-		int value = -1;
-		int status = str_to_int("", &value);
-		fail_unless (status == 0);
-		fail_unless (value == -1);
-	}
-	END_TEST
+START_TEST (test_str_to_int_empty_string)
+{
+	int value = -1;
+	int status = str_to_int("", &value);
+	fail_unless (status == 0);
+	fail_unless (value == -1);
+}
+END_TEST
 
-	START_TEST (test_str_to_int_invalid_string)
-	{
-		int value = -1;
-		int status = str_to_int("-123a4", &value);
-		fail_unless (status == 0);
-		fail_unless (value == -1);
-	}
-	END_TEST
+START_TEST (test_str_to_int_invalid_string)
+{
+	int value = -1;
+	int status = str_to_int("-123a4", &value);
+	fail_unless (status == 0);
+	fail_unless (value == -1);
+}
+END_TEST
 
-	START_TEST (test_int_to_str)
-	{
-		int value = 123;
-		char *expected = "123";
-		char *actual = calloc(10, sizeof(char));
-		int status = int_to_str(value, &actual);
-		fail_unless(status == 1, "status = %d", status);
-		fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
-		free(actual);
-	}
-	END_TEST
-
-	START_TEST (test_int_to_str_negative_value)
-	{
-		int value = -123;
-		char *expected = "-123";
-		char *actual = calloc(10, sizeof(char));
-		int status = int_to_str(value, &actual);
-		fail_unless(status == 1, "status = %d", status);
-		fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
+START_TEST (test_int_to_str)
+{
+	int value = 123;
+	char *expected = "123";
+	char *actual = calloc(10, sizeof(char));
+	int status = int_to_str(value, &actual);
+	fail_unless(status == 1, "status = %d", status);
+	fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
 	free(actual);
+}
+END_TEST
+
+START_TEST (test_int_to_str_negative_value)
+{
+	int value = -123;
+	char *expected = "-123";
+	char *actual = calloc(10, sizeof(char));
+	int status = int_to_str(value, &actual);
+	fail_unless(status == 1, "status = %d", status);
+	fail_unless(strcmp(actual, expected) == 0, "%s != %s", actual, expected);
+free(actual);
 }
 END_TEST
 
@@ -328,7 +328,7 @@ START_TEST(test_char_most_repeated_one_char)
 	int count = -1;
 	char *str = "a";
 	int status = char_most_repeated(str, &output, &index, &count);
-
+	
 	fail_unless(status == 1);
 	fail_unless(output == 'a');
 	fail_unless(index == 0);
@@ -336,6 +336,234 @@ START_TEST(test_char_most_repeated_one_char)
 }
 END_TEST
 
+START_TEST (test_first_char_m_times_ocurred_1)
+{
+	int index = -1;
+	char output = '\0';
+	char *str = "abcdbcd";
+	int status = first_char_m_times_occurred(str, 1, &output, &index);
+
+	fail_unless(status == 1);
+	fail_unless(output == 'a');
+	fail_unless(index == 0);	
+}
+END_TEST
+
+START_TEST (test_first_char_m_times_ocurred_2_at_the_end)
+{
+	int index = -1;
+	char output = '\0';
+	char *str = "abbbccca";
+	int status = first_char_m_times_occurred(str, 2, &output, &index);
+
+	fail_unless(status == 1);
+	fail_unless(output == 'a');
+	fail_unless(index == 0);	
+}
+END_TEST
+
+START_TEST (test_first_char_m_times_ocurred_non_existent)
+{
+	int index = -1;
+	char output = '\0';
+	char *str = "abcdbcd";
+	int status = first_char_m_times_occurred(str, 3, &output, &index);
+
+	fail_unless(status == 0);
+	fail_unless(output == '\0');
+	fail_unless(index == -1);	
+}
+END_TEST
+
+START_TEST (test_first_char_m_times_ocurred_null)
+{
+	int index = -1;
+	char output = '\0';
+	char *str = NULL;
+	int status = first_char_m_times_occurred(str, 1, &output, &index);
+
+	fail_unless(status == 0);
+	fail_unless(output == '\0');
+	fail_unless(index == -1);	
+}
+END_TEST
+
+START_TEST (test_first_char_m_times_ocurred_empty_string)
+{
+	int index = -1;
+	char output = '\0';
+	char *str = "";
+	int status = first_char_m_times_occurred(str, 1, &output, &index);
+
+	fail_unless(status == 0);
+	fail_unless(output == '\0');
+	fail_unless(index == -1);	
+}
+END_TEST
+
+START_TEST (test_first_char_m_times_ocurred_0)
+{
+	int index = -1;
+	char output = '\0';
+	char *str = "";
+	int status = first_char_m_times_occurred(str, 0, &output, &index);
+
+	fail_unless(status == 0);
+	fail_unless(output == '\0');
+	fail_unless(index == -1);	
+}
+END_TEST
+
+START_TEST (test_first_char_m_times_ocurred_negative_m)
+{
+	int index = -1;
+	char output = '\0';
+	char *str = "";
+	int status = first_char_m_times_occurred(str, -1, &output, &index);
+
+	fail_unless(status == 0);
+	fail_unless(output == '\0');
+	fail_unless(index == -1);	
+}
+END_TEST
+
+START_TEST (test_remove_quotes)
+{
+	char *str = strdup("\"where there is a sea, there pirates be\"");
+	char *exp = "";
+	int status = remove_quotes(str);
+	fail_unless(status == 1);
+	fail_unless(strcmp(str, exp) == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_begining_1)
+{
+	char *str = strdup("\"walk ok\" he said");
+	char *exp = " he said";
+	int status = remove_quotes(str);
+	fail_unless(status == 1);
+	fail_unless(strcmp(str, exp) == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_begining_2)
+{
+	char *str = strdup("t\"walk ok\" he said");
+	char *exp = "t he said";
+	int status = remove_quotes(str);
+	fail_unless(status == 1);
+	fail_unless(strcmp(str, exp) == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_middle)
+{
+	char *str = strdup("he said \"walk on\" to me.");
+	char *exp = "he said  to me.";
+	int status = remove_quotes(str);
+	fail_unless(status == 1);
+	fail_unless(strcmp(str, exp) == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_end_1)
+{
+	char *str = strdup("he said \"walk on\"");
+	char *exp = "he said ";
+	int status = remove_quotes(str);
+	fail_unless(status == 1);
+	fail_unless(strcmp(str, exp) == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_end_2)
+{
+	char *str = strdup("he said \"walk on\".");
+	char *exp = "he said .";
+	int status = remove_quotes(str);
+	fail_unless(status == 1);
+	fail_unless(strcmp(str, exp) == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_many_quotes)
+{
+	char *str = strdup("1\"2\"3\"4\"5\"6\"7");
+	char *exp = "1357";
+	int status = remove_quotes(str);
+	fail_unless(status == 1);
+	fail_unless(strcmp(str, exp) == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_not_well_formed_quotes)
+{
+	char *str = strdup("1\"2");
+	char *exp = "1\"2";
+	int status = remove_quotes(str);
+	fail_unless(status == 0);
+	fail_unless(strcmp(str, exp) == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_no_quotes)
+{
+	char *str = strdup("Hello Hello, I'm at a place called vertigo");
+	char *exp = "Hello Hello, I'm at a place called vertigo";
+	int status = remove_quotes(str);
+	fail_unless(status == 0);
+	fail_unless(strcmp(str, exp) == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_empty_string)
+{
+	char *str = strdup("");
+	char *exp = "";
+	int status = remove_quotes(str);
+	fail_unless(status == 0);
+	fail_unless(strcmp(str, exp) == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_null_string)
+{
+	char *str = NULL;
+	int status = remove_quotes(str);
+	fail_unless(status == 0);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_one_char)
+{
+	char *str = strdup("a");
+	int status = remove_quotes(str);
+	fail_unless(status == 0);
+	fail_unless(strcmp(str, "a") == 0);
+	free(str);
+}
+END_TEST
+
+START_TEST (test_remove_quotes_only_quotes)
+{
+	char *str = strdup("\"\"");
+	int status = remove_quotes(str);
+	fail_unless(status == 1);
+	fail_unless(strcmp(str, "") == 0);
+	free(str);
+}
+END_TEST
 
 Suite *get_suite(void)
 {
@@ -386,6 +614,21 @@ Suite *get_suite(void)
 	tcase_add_test(tc_char_most_repeated, test_char_most_repeated_one_char);
 	suite_add_tcase(s, tc_char_most_repeated);
 	
+	TCase *tc_remove_quotes = tcase_create("tcase_remove_quotes");
+	tcase_add_test(tc_remove_quotes, test_remove_quotes);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_begining_1);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_begining_2);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_middle);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_end_1);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_end_2);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_many_quotes);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_not_well_formed_quotes);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_no_quotes);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_empty_string);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_null_string);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_one_char);
+	tcase_add_test(tc_remove_quotes, test_remove_quotes_only_quotes);
+	suite_add_tcase(s, tc_remove_quotes);
 	
 	return s;
 }
