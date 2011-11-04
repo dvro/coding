b884fb072517f478a7cc4a5336bb8d3f9b7b7241
Dayvid Victor <victor.dvro@gmail.com>
2011-11-04 11:16:20 -0300

    array_utils.c - max_subsum (not tested yet)

--- algorithms/lib/array_utils.c
+++ algorithms/lib/array_utils.c
@@ -4,9 +4,35 @@
 #include <array_utils.h>
 
 
-int max_subarray_sum (int array[], int len, int *max_subsum)
+int max_subarray_sum (int array[], int len, int *ini, int *end)
 {
+	
+	int max_sum = -1;
+	int max_ini = -1;
+	int max_end = -1;
+	
+	int cur_sum = 0;
+	int cur_ini = 0;
+	int cur_end = 0;
 
+	max_sum = array[0];
+
+	int i;
+	for (i = 0; i < len; i++) {
+		cur_sum = cur_sum + array[i];
+		if (cur_sum > max_sum) {
+			int max_sum = cur_sum;
+			int max_ini = cur_ini;
+			int max_end = i;
+		}
+		
+		if (cur_sum < 0) {
+			cur_ini = i + 1;
+			cur_sum = 0;	
+		}
+	}
+
+	return 1;
 }
 
 int binary_search (int array[], int len, int value, int *index)
