c487d473dc13945b139ce1851a01333a75afe287
Dayvid Victor <victor.dvro@gmail.com>
2011-11-04 11:04:10 -0300

    array_utils.* - created

---
+++ algorithms/include/array_utils.h
@@ -0,0 +1,12 @@
+#ifndef ARRAY_UTILS_H
+#define ARRAY_UTILS_H
+#include <stdlib.h>
+#include <stdio.h>
+
+int max_subarray_sum (int array[], int len, int *max_subsum);
+int binary_search (int array[], int len, int value, int *index);
+int quick_sort (int *array, int len);
+int merge_sort (int *array, int len);
+
+#endif
+
---
+++ algorithms/lib/array_utils.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <array_utils.h>
+
+
+int max_subarray_sum (int array[], int len, int *max_subsum)
+{
+
+}
+
+int binary_search (int array[], int len, int value, int *index)
+{
+
+}
+
+int quick_sort (int *array, int len)
+{
+
+}
+
+int merge_sort (int *array, int len)
+{
+
+}
